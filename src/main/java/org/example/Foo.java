8544d34e4169ca4e2d8bcde25b109c30d946af14
Vadik <gloom16@mail.ru>
2024-03-01 05:34:36 +0300

    init commit

--- src/main/java/org/example/Foo.java
+++ src/main/java/org/example/Foo.java
@@ -1,15 +1,38 @@
 package org.example;
 
+import java.util.concurrent.CountDownLatch;
+
 public class Foo {
+    private final CountDownLatch cd1 = new CountDownLatch(1);
+    private final CountDownLatch cd2 = new CountDownLatch(1);
+
     public void first() {
+        new MyThread(cd1);
         System.out.print("first");
+        cd1.countDown();
     }
 
     public void second() {
-        System.out.print("second");
+        new MyThread(cd1);
+
+        try {
+            cd1.await();
+            System.out.print("second");
+            cd2.countDown();
+        } catch (InterruptedException e) {
+            System.out.println("Поток прерван" + e);
+        }
     }
 
     public void third() {
-        System.out.println("third");
+        new MyThread(cd2);
+
+        try {
+            cd2.await();
+            System.out.print("third");
+            cd2.countDown();
+        } catch (InterruptedException e) {
+            System.out.println("Поток прерван" + e);
+        }
     }
 }
