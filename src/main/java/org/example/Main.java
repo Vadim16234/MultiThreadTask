243b5be58b7b872b563c8f2f16ea36ddd0c237cd
Vadik <gloom16@mail.ru>
2024-03-01 05:33:47 +0300

    init commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,14 +1,12 @@
 package org.example;
 
-import java.util.concurrent.Semaphore;
+import java.util.concurrent.CompletableFuture;
 
 public class Main {
     public static void main(String[] args) {
         Foo foo = new Foo();
-        Semaphore sem = new Semaphore(1);
-
-        new FirstThread(sem, foo);
-        new SecondThread(sem, foo);
-        new ThirdThread(sem, foo);
+        CompletableFuture.runAsync(() -> foo.third());
+        CompletableFuture.runAsync(() -> foo.first());
+        CompletableFuture.runAsync(() -> foo.second());
     }
 }
