335bf1e0831aed21c0a3b31a89f71d2dbf2b5ab5
Vadik <gloom16@mail.ru>
2024-03-03 22:54:04 +0300

    init commit

--- src/main/java/org/example/Foo.java
+++ src/main/java/org/example/Foo.java
@@ -2,41 +2,51 @@ package org.example;
 
 import java.util.concurrent.CountDownLatch;
 
-public class Foo implements Runnable{
+public class Foo {
     private final CountDownLatch cd1 = new CountDownLatch(1);
     private final CountDownLatch cd2 = new CountDownLatch(1);
 
     public void first() {
-        new Thread();
-        System.out.print("first");
+        Thread threadA = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.print("first");
+            }
+        });
+
+        threadA.start();
         cd1.countDown();
     }
 
     public void second() {
-        new Thread();
-
-        try {
-            cd1.await();
-            System.out.print("second");
-            cd2.countDown();
-        } catch (InterruptedException e) {
-            System.out.println("Поток прерван" + e);
-        }
+        Thread threadB = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    cd1.await();
+                    System.out.print("second");
+                    cd2.countDown();
+                } catch (InterruptedException e) {
+                    System.out.println("Поток прерван" + e);
+                }
+            }
+        });
+        threadB.start();
     }
 
     public void third() {
-        new Thread();
-
-        try {
-            cd2.await();
-            System.out.print("third");
-            cd2.countDown();
-        } catch (InterruptedException e) {
-            System.out.println("Поток прерван" + e);
-        }
-    }
-
-    @Override
-    public void run() {
+        Thread threadC = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    cd2.await();
+                    System.out.print("third");
+                    cd2.countDown();
+                } catch (InterruptedException e) {
+                    System.out.println("Поток прерван" + e);
+                }
+            }
+        });
+        threadC.start();
     }
 }
